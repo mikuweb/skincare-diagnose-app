ad466552923e147e9a6c8af57f9aecdf5cfcba95
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-03-22 15:21:20 +0900

    style: 全体のmax-widthを指定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import KeyVisual from "@/components/KeyVisual";
 
 export default function Home() {
   return (
-    <>
+    <div className="max-w-screen-2xl mx-auto">
       <KeyVisual />
       <Contents />
-    </>
+    </div>
   );
 }
