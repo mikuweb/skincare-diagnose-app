d9be1078dbe9bbefa1c8d6c613e006d7a9320182
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-03-22 17:28:28 +0900

    style: レスポンシブ化

--- src/components/Contents/ContentItem/index.tsx
+++ src/components/Contents/ContentItem/index.tsx
@@ -19,20 +19,18 @@ const ContentItem: FC<Props> = ({
     <div
       style={{
         backgroundImage: `url('/images/${imgSrc}')`,
-        width: "270px",
-        height: "304px",
       }}
-      className="bg-no-repeat bg-cover rounded-3xl flex flex-col justify-end items-start  py-6 px-4 gap-4 cursor-pointer"
+      className="w-72 h-80 lg:h-full grow bg-no-repeat bg-cover rounded-3xl flex flex-col justify-end items-start  py-6 px-4 gap-4 cursor-pointer"
       onClick={movePage}
     >
-      <p className="text-white text-xs text-left">
+      <p className="text-white text-xs text-left md:text-lg">
         {description1}
         <br />
         {description2}
       </p>
       <button
         type="button"
-        className="bg-white py-1 px-4 rounded-lg text-xs text-brown-100 w-fit hover:opacity-80"
+        className="bg-white py-1 px-4 rounded-lg text-xs text-brown-100 w-fit hover:opacity-80 md:text-lg md:px-6 md:py-3"
       >
         {btnText}
       </button>
--- src/components/Contents/index.tsx
+++ src/components/Contents/index.tsx
@@ -10,26 +10,40 @@ const Contents = () => {
   };
 
   return (
-    <div className="py-7 px-4 text-sm text-center">
-      <p className="mb-6">
-        自分の肌タイプ・お悩みに合わせたスキンケアで美しいお肌に
-      </p>
-      <div className="flex flex-col items-center gap-14">
-        <ContentItem
-          imgSrc={"skin-type.jpg"}
-          description1={"お肌に関する質問に答えて"}
-          description2={"まずはあなたの肌タイプを診断"}
-          btnText={"肌タイプ診断を受ける"}
-          movePage={() => movePage("/skin-type")}
-        />
+    <div className="py-7 px-4 text-sm text-center lg:flex lg:flex-row lg:justify-center lg:gap-9 lg:py-20">
+      {/* 左 スキンケアアイテム画像 */}
+      <img
+        src="/images/skin-care-items.jpg"
+        alt="スキンケアアイテム"
+        sizes="100%"
+        className="hidden w-full h-fit rounded-3xl lg:block lg:w-1/3"
+      />
 
-        <ContentItem
-          imgSrc={"skin-care-product.jpg"}
-          description1={"お肌に関する質問に答えるだけで"}
-          description2={"AI分析によるおすすめスキンケア成分を提案"}
-          btnText={"おすすめスキンケア診断を受ける"}
-          movePage={() => movePage("/")}
-        />
+      {/* 右 コンテンツ */}
+      <div className="lg:w-2/3 lg:flex lg:flex-col">
+        <p className="mb-6 mx-auto w-4/5 md:text-2xl md:mb-7 lg:text-2xl lg:w-full">
+          まずは自分のお肌タイプを知り、
+          <br />
+          それぞれのお肌悩みに合わせたスキンケアで <br />{" "}
+          美しいお肌に導きましょう
+        </p>
+        <div className="flex flex-col items-center w-full gap-14 mx-auto md:flex-row md:gap-8 md:justify-center lg:gap-5 lg:h-full">
+          <ContentItem
+            imgSrc={"skin-type.jpg"}
+            description1={"お肌に関する質問に答えて"}
+            description2={"まずはあなたの肌タイプを診断"}
+            btnText={"肌タイプ診断を受ける"}
+            movePage={() => movePage("/skin-type")}
+          />
+
+          <ContentItem
+            imgSrc={"skin-care-product.jpg"}
+            description1={"お肌に関する質問に答えるだけで"}
+            description2={"AI分析によるおすすめスキンケア成分を提案"}
+            btnText={"スキンケア提案を受ける"}
+            movePage={() => movePage("/")}
+          />
+        </div>
       </div>
     </div>
   );
