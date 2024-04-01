d7a5452e97375ba4d6b61f487f9ec5c0c76e3a6a
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-04-01 15:19:15 +0900

    feat: 質問にアニメーションをつける

--- src/components/Questions/Question.tsx
+++ src/components/Questions/Question.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { QuestionType } from "./Wizard";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface Props {
   questions: QuestionType[];
@@ -9,19 +10,50 @@ interface Props {
 const Question: FC<Props> = ({ questions, activeQuestion }) => {
   return (
     <>
-      <div className="absolute left-0 right-0 w-64 h-64 mx-auto bg-beige-100 bg-opacity-60"></div>
       <div
         style={{
           backgroundImage: `url('/images/blue-flower_1.png')`,
           height: "256px",
           width: "256px",
         }}
-        className="bg-no-repeat bg-cover bg-opacity-60 px-10 mx-auto flex flex-col gap-5 justify-center items-center "
+        className="relative bg-no-repeat bg-cover bg-opacity-60 px-10 mx-auto flex flex-col gap-5 justify-center items-center "
       >
-        <p className="text-leaf-300 text-xl z-10">Q{activeQuestion + 1}</p>
-        <p className="text-center text-brown-10 text-sm z-10">
-          {questions[activeQuestion].question}
-        </p>
+        <AnimatePresence initial={false}>
+          <motion.p
+            key={questions[activeQuestion].question + "1"}
+            initial={{ opacity: 0, translateY: 15 }}
+            animate={{
+              opacity: 1,
+              translateY: 0,
+              transition: { duration: 1, delay: 0.2 },
+            }}
+            exit={{
+              opacity: 0,
+              translateY: -10,
+              transition: { duration: 0.2 },
+            }}
+            className="text-leaf-300 text-xl z-10 absolute top-16"
+          >
+            Q{activeQuestion + 1}
+          </motion.p>
+          <motion.p
+            key={questions[activeQuestion].question}
+            initial={{ opacity: 0, translateY: 20 }}
+            animate={{
+              opacity: 1,
+              translateY: 0,
+              transition: { duration: 0.8, delay: 0.4 },
+            }}
+            exit={{
+              opacity: 0,
+              translateY: -10,
+              transition: { duration: 0.2 },
+            }}
+            className="w-40 text-center text-brown-10 text-sm z-10 absolute top-28"
+          >
+            {questions[activeQuestion].question}
+          </motion.p>
+        </AnimatePresence>
       </div>
     </>
   );
