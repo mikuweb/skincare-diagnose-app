bc37510a87059352cd383450228089798fecd1b1
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-06-13 18:37:05 +0900

    feat: implement skincare-product API

--- src/app/skin-care-product/page.tsx
+++ src/app/skin-care-product/page.tsx
@@ -1,30 +1,42 @@
 import Wizard, { QuestionType } from "@/components/Questions/Wizard";
+import { querySkinCareProduct } from "@/features/api/actions";
 import { NextPage } from "next";
 import React from "react";
-import { querySkinCareProduct } from "../actions";
 
 const skinCareProductQuestions: QuestionType[] = [
   {
     id: 0,
-    question: "今一番気になっているお肌の悩みに近いものを選んでください。",
-    choices: ["シミ", "毛穴", "ニキビ", "乾燥"],
+    question: "あなたの肌質を教えてください",
+    choices: ["乾燥肌", "脂性肌", "混合肌", "敏感肌", "わからない"],
   },
   {
     id: 1,
-    question:
-      "ここに質問が表示されます。ここに質問が表示されます。ここに質問が表示されます。",
-    choices: ["回答選択肢", "回答選択肢", "回答選択肢", "回答選択肢"],
+    question: "今一番気になっているお肌の悩みに近いものを選んでください。",
+    choices: ["シミ", "シワ", "毛穴", "ニキビ", "乾燥", "くすみ"],
+    // TODO: テキスト入力もできるようにする
+    // その他（具体的な悩みがあれば教えてください）
   },
   {
     id: 2,
-    question: "普段お使いのスキンケアアイテムを教えてください",
-    choices: ["回答選択肢", "回答選択肢", "回答選択肢", "回答選択肢"],
+    question: "理想の肌はどのような状態ですか？",
+    choices: [
+      "ツヤツヤでハリのある肌",
+      "シミやシワのない透明感のある肌",
+      "ニキビのないなめらかな肌",
+      "乾燥を感じないもちもちの肌",
+      // TODO: テキスト入力もできるようにする
+      // その他（具体的なイメージがあれば教えてください）
+    ],
   },
   {
     id: 3,
-    question:
-      "ここに質問が表示されます。ここに質問が表示されます。ここに質問が表示されます。",
-    choices: ["回答選択肢", "回答選択肢", "回答選択肢", "回答選択肢"],
+    question: "アレルギーやアトピーなどの皮膚疾患はありますか？",
+    choices: ["はい", "いいえ"],
+  },
+  {
+    id: 4,
+    question: "妊娠中・授乳中ですか？",
+    choices: ["はい", "いいえ"],
   },
 ];
 
--- src/features/api/actions.ts
+++ src/features/api/actions.ts
@@ -51,3 +51,43 @@ export async function querySkinType(reqestQuery: string[]) {
     console.error("エラー：", error);
   }
 }
+
+export async function querySkinCareProduct(reqestQuery: string[]) {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = `
+  肌の専門家として、以下の質問への回答を参考に、質問者の肌悩みにあった科学的根拠のあるスキンケア成分と、その成分が配合されたおすすめの市販スキンケア商品を出力フォーマットに沿って回答してください。
+
+  Q:${reqestQuery[0]}
+  A:${reqestQuery[1]}
+
+  Q:${reqestQuery[2]}
+  A:${reqestQuery[3]}
+
+  Q:${reqestQuery[4]}
+  A:${reqestQuery[5]}
+
+  Q:${reqestQuery[6]}
+  A:${reqestQuery[7]}
+
+  Q:${reqestQuery[8]}
+  A:${reqestQuery[9]}
+
+  出力フォーマット
+  ## おすすめスキンケア成分：  ** 回答 **
+  
+  ## おすすめスキンケア商品
+    - 回答
+    - 回答
+      `;
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  try {
+    return text;
+  } catch (error) {
+    console.error("エラー：", error);
+  }
+}
