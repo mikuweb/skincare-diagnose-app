675bf7bb6122249147635c33d5f2d2ca76d8ca40
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-06-17 17:40:55 +0900

    remove await

--- src/features/api/actions.ts
+++ src/features/api/actions.ts
@@ -44,7 +44,7 @@ export async function querySkinType(reqestQuery: string[]) {
 
   try {
     const result = await model.generateContent(prompt);
-    const response = await result.response;
+    const response = result.response;
     const text = response.text();
     return text;
   } catch (error) {
