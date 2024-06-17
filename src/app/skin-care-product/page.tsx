0d29ef55a44f3de02776fb30d70e1277b81dd526
Miku Kawata <mxxx.kwtmg@gmail.com>
2024-06-17 17:12:30 +0900

    move questions into another file and import them

--- src/app/skin-care-product/page.tsx
+++ src/app/skin-care-product/page.tsx
@@ -1,44 +1,8 @@
-import Wizard, { QuestionType } from "@/components/Questions/Wizard";
+import Wizard from "@/components/Questions/Wizard";
 import { querySkinCareProduct } from "@/features/api/actions";
+import { skinCareProductQuestions } from "@/lib/questions";
 import { NextPage } from "next";
 import React from "react";
-
-const skinCareProductQuestions: QuestionType[] = [
-  {
-    id: 0,
-    question: "あなたの肌質を教えてください",
-    choices: ["乾燥肌", "脂性肌", "混合肌", "敏感肌", "わからない"],
-  },
-  {
-    id: 1,
-    question: "今一番気になっているお肌の悩みに近いものを選んでください。",
-    choices: ["シミ", "シワ", "毛穴", "ニキビ", "乾燥", "くすみ"],
-    // TODO: テキスト入力もできるようにする
-    // その他（具体的な悩みがあれば教えてください）
-  },
-  {
-    id: 2,
-    question: "理想の肌はどのような状態ですか？",
-    choices: [
-      "ツヤツヤでハリのある肌",
-      "シミやシワのない透明感のある肌",
-      "ニキビのないなめらかな肌",
-      "乾燥を感じないもちもちの肌",
-      // TODO: テキスト入力もできるようにする
-      // その他（具体的なイメージがあれば教えてください）
-    ],
-  },
-  {
-    id: 3,
-    question: "アレルギーやアトピーなどの皮膚疾患はありますか？",
-    choices: ["はい", "いいえ"],
-  },
-  {
-    id: 4,
-    question: "妊娠中・授乳中ですか？",
-    choices: ["はい", "いいえ"],
-  },
-];
 
 const SkinCareProductPage: NextPage = () => {
   return (
--- src/app/skin-type/page.tsx
+++ src/app/skin-type/page.tsx
@@ -1,43 +1,9 @@
 import Wizard from "@/components/Questions/Wizard";
-import { QuestionType } from "@/components/Questions/Wizard";
 import { NextPage } from "next";
 import { querySkinType } from "@/features/api/actions";
+import { skinTypeQuestions } from "@/lib/questions";
 
 const SkinTypePage: NextPage = () => {
-  const skinTypeQuestions: QuestionType[] = [
-    {
-      id: 0,
-      // When I touch my forehead, nose and cheeks with the back of my hand,
-      question:
-        "手の甲でTゾーン(おでこ・鼻)や頬を触ってみましょう。どこのベタつきが気になりますか？",
-      choices: [
-        "ほとんど気にならない",
-        "Tゾーンだけ気になる",
-        "全体的に気になる",
-      ],
-    },
-    {
-      id: 1,
-      question: "洗顔後に何も塗らない状態で肌のつっぱりを感じますか？",
-      choices: [
-        "つっぱりは感じない",
-        "少しつっぱりを感じる",
-        "常につっぱりを感じる",
-      ],
-    },
-    {
-      id: 2,
-      question: "肌が乾燥してかゆみや粉ふきが起こりますか？",
-      choices: ["頻繁に起こる", "時々起こる", "起こらない"],
-    },
-    {
-      id: 3,
-      question:
-        "化粧品や外部刺激に対して肌が赤くなったり痛みを感じたり敏感に炎症を起こしますか？",
-      choices: ["頻繁に起こす", "時々起こす", "起こさない"],
-    },
-  ];
-
   return (
     <Wizard
       questionList={skinTypeQuestions}
